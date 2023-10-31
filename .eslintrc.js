9af3776ed3cc19dd94d72b7102d529d91edaf09a
“Chiaki” <chias.926@gmail.com>
2023-10-31 22:32:39 +0900

    station1-3 修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,23 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  globals: {
+    test: false,
+    expect: false,
+  },
+  rules: { 'react/prop-types': 'off' },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "prettier"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "react"
-    ],
-    "rules": {
-    },
-    "settings": {
-        "react": {
-          "version": "detect"
-        }
-      },
-}
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import "./index.scss";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { CookiesProvider } from "react-cookie";
-import { Provider } from "react-redux";
-import { store } from "./store"
+import './index.scss';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { CookiesProvider } from 'react-cookie';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
