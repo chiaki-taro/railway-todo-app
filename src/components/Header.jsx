38ec69bc5409aa06bc11b71f774fbae32e8b266e
“Chiaki” <chias.926@gmail.com>
2023-11-02 21:21:19 +0900

    記述修正など

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
 import { useSelector, useDispatch } from 'react-redux/es/exports';
 import { useNavigate } from 'react-router-dom';
@@ -8,17 +9,23 @@ import './header.scss';
 export const Header = () => {
   const auth = useSelector((state) => state.auth.isSignIn);
   const dispatch = useDispatch();
-  const history = useNavigate();
-  const [removeCookie] = useCookies();
+  const navigate = useNavigate();
+  // eslint-disable-next-line no-unused-vars
+  const [cookies, setCookie, removeCookie] = useCookies();
+  const navigateHome = () => {
+    navigate('/');
+  }
   const handleSignOut = () => {
     dispatch(signOut());
     removeCookie('token');
-    history.push('/signin');
+    navigate('/signin');
   };
 
   return (
     <header className="header">
-      <h1>Todoアプリ</h1>
+      <Link to="/" className="home-link">
+        <h1>Todoアプリ</h1>
+      </Link>
       {auth ? (
         <button onClick={handleSignOut} className="sign-out-button">
           サインアウト
